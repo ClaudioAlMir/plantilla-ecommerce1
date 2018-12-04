a98591df90d6ddb3399f49ed2b3f587a12eeb2e4
Jorge <jorgeoscargamez@gmail.com>
2018-12-04 19:59:48 -0300

    https://github.com/drehimself/laravel-ecommerce-example.git

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Ecommerce</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat%7CRoboto:300,400,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+    </head>
+    <body>
+        <div id="app">
+            <header class="with-background">
+                <div class="top-nav container">
+                    <div class="top-nav-left">
+                        <div class="logo">Ecommerce</div>
+                        {{ menu('main', 'partials.menus.main') }}
+                    </div>
+                    <div class="top-nav-right">
+                        @include('partials.menus.main-right')
+                    </div>
+                </div> <!-- end top-nav -->
+                <div class="hero container">
+                    <div class="hero-copy">
+                        <h1>Laravel Ecommerce</h1>
+                        <p>Includes multiple products, categories, a shopping cart and a checkout system with Stripe integration.</p>
+                        <div class="hero-buttons">
+                            <a href="https://www.youtube.com/playlist?list=PLEhEHUEU3x5oPTli631ZX9cxl6cU_sDaR" class="button button-white">Screencasts</a>
+                            <a href="https://github.com/drehimself/laravel-ecommerce-example" class="button button-white">GitHub</a>
+                        </div>
+                    </div> <!-- end hero-copy -->
+
+                    <div class="hero-image">
+                        <img src="img/macbook-pro-laravel.png" alt="hero image">
+                    </div> <!-- end hero-image -->
+                </div> <!-- end hero -->
+            </header>
+
+            <div class="featured-section">
+
+                <div class="container">
+                    <h1 class="text-center">Laravel Ecommerce</h1>
+
+                    <p class="section-description">El comercio electrónico se ha convertido en uno de los rubros más demandados en el mercado laboral. Cada vez son más los empresarios que se sienten tentados a invertir en la red para obtener grandes ganancias.
+
+Pero la dinámica de compra-venta en Internet requiere de una formación técnica que permita el dominio de las directrices más actuales para diferenciarse frente a la competencia y lograr el impacto deseado en el público consumidor.</p>
+
+                    <div class="text-center button-container">
+                        <a href="#" class="button">Featured</a>
+                        <a href="#" class="button">On Sale</a>
+                    </div>
+
+                    {{-- <div class="tabs">
+                        <div class="tab">
+                            Featured
+                        </div>
+                        <div class="tab">
+                            On Sale
+                        </div>
+                    </div> --}}
+
+                    <div class="products text-center">
+                        @foreach ($products as $product)
+                            <div class="product">
+                                <a href="{{ route('shop.show', $product->slug) }}"><img src="{{ productImage($product->image) }}" alt="product"></a>
+                                <a href="{{ route('shop.show', $product->slug) }}"><div class="product-name">{{ $product->name }}</div></a>
+                                <div class="product-price">{{ $product->presentPrice() }}</div>
+                            </div>
+                        @endforeach
+
+                    </div> <!-- end products -->
+
+                    <div class="text-center button-container">
+                        <a href="{{ route('shop.index') }}" class="button">View more products</a>
+                    </div>
+
+                </div> <!-- end container -->
+
+            </div> <!-- end featured-section -->
+
+            <blog-posts></blog-posts>
+
+            @include('partials.footer')
+
+        </div> <!-- end #app -->
+        <script src="js/app.js"></script>
+    </body>
+</html>
